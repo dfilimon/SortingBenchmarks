3893726459bf4816550f38df08882b3422ed4452
Dan-George Filimon <Dan@Dan-George-Filimons-MacBook-Pro.local>
2010-10-17 12:32:26 +0300

    Final version. Added README and COPYING info. Fixed octave input processing.

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,24 @@
 #! /usr/bin/env python
 
+"""
+Author: Dan-George Filimon
+
+This file is part of SortingBenchmarks.
+
+SortingBenchmarks is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 import os
 import string
 import subprocess
@@ -14,10 +33,17 @@ algorithms = ['mergesort',
               ,'insertionsort',
               'bubblesort']
 """
+
+"""
+Modify these parameters for different benchmarks.
+I was too lazy to implement input parsing.
+"""
 numMax = 1000000
 numSteps = 500
+deleteBenchmarks = False
 
 def main():
+
     # compile java code
     java = subprocess.Popen(['javac', 'SortingBenchmarks.java'])
     java.wait()
@@ -31,13 +57,12 @@ def main():
     octave = subprocess.Popen(['./draw_plots.m'])
     octave.wait()
 
-    """
-    # clean up nasty benchmark files
-    for fileName in os.listdir('.'):
-        pos = string.rfind(fileName, '.benchmark')
-        if pos >= 0 and pos == len(fileName) - len('.benchmark'):
+    if deleteBenchmarks:
+        # clean up nasty benchmark files
+        for fileName in os.listdir('.'):
+            pos = string.rfind(fileName, '.benchmark')
+            if pos >= 0 and pos == len(fileName) - len('.benchmark'):
             os.remove(fileName)
-    """
     
 if __name__ == "__main__":
     main()
--- draw_plots.m
+++ draw_plots.m
@@ -1,38 +1,56 @@
 #! /usr/bin/env octave -q
 1;
 
+%{ 
+Author: Dan-George Filimon
+
+This file is part of SortingBenchmarks.
+
+SortingBenchmarks is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/ ...
+        licenses/>.
+%}
+
+% be careful, textread is in the octave-io package!
+
 function drawPlots(prefix)
-  colors = "rgbmck";  
-  files = dir([prefix, ".*.benchmark"]); 
+  colors = 'rgbmck';  
+  files = dir([prefix, '.*.benchmark']); 
   
   clf;
   hold on;
 
   for i = 1 : length(files)
-    printf("%s %s\n", files(i).name(), colors(i));
-    fileHandle = fopen(files(i).name, "r");
-    all = fscanf(fileHandle, "%d", 500);
-    x = all(1 : 2 : end);
-    y = all(2 : 2 : end);
+    printf('%s %s\n', files(i).name(), colors(i));
+    [x, y] = textread(files(i).name, '%d', 2);
+    %p = polyfit(x, y, 2);    
+    %plot(x, polyval(p, x), colors(i), 'LineWidth', 1)
+    plot(x, y, [colors(i), 'x'], 'MarkerSize', 2);
 
-    #p = polyfit(x, y, 2);    
-    #plot(x, polyval(p, x), [colors(i), ""], "LineWidth", 1)
-    plot(x, y, [colors(i), "x"], "MarkerSize", 2)
-
-    xlabel("Number of elements");
-    ylabel("Nanoseconds");
+    xlabel('Number of elements');
+    ylabel('Nanoseconds');
   end
-  title([prefix, " benchmarks"]);
+  title([prefix, ' benchmarks']);
   legend(files.name)
-  print([prefix, ".benchmarks.png"], "-dpng");
+  print([prefix, '.benchmarks.png'], '-dpng');
 
   hold off;
   pause();
 end
 
 # plotting benchmarks with random data
-drawPlots("rand")
+drawPlots('rand')
 
 # plotting benchmarks with worstcase data
-drawPlots("worstcase")
+drawPlots('worstcase')
   
